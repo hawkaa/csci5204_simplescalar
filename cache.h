8a2392cea24e0f607210de579c8d19e4269628c5
Håkon Åmdal <hakon@aamdal.com>
2014-11-08 13:58:30 -0600

    Add bip fields to cache

--- cache.h
+++ cache.h
@@ -158,6 +158,8 @@ struct cache_t
   int assoc;			/* cache associativity */
   enum cache_policy policy;	/* cache replacement policy */
   unsigned int hit_latency;	/* cache hit latency */
+  int bip_counter;
+  int bip_counter_limit;
 
   /* miss/replacement handler, read/write BSIZE bytes starting at BADDR
      from/into cache block BLK, returns the latency of the operation
